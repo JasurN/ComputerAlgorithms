cdfecfdeecb22a39d8d2787489b1874d860ca9b0
Jasurbek Nabijonov <97jasur@gmail.com>
2017-11-22 18:45:21 +0500

    1. Implemented Insertion Sort (NOT TESTED)

--- cpp/src/sorting/InsertionSort.cpp
+++ cpp/src/sorting/InsertionSort.cpp
@@ -1,1 +1,50 @@
+#include "InsertionSort.hpp"
+
+InsertionSort::InsertionSort() {
+    Input inputObj;
+
+    unsortedArray = inputObj.takeInput();
+
+    doInsertionSort(unsortedArray);
+}
+
+void InsertionSort::doInsertionSort(std::vector<int>array) {
+    int key = 0;
+    int i = 0;
+
+    for(int j = 1; j < array.size(); ++j) {
+        key = array[j];
+
+        i = j - 1;
+
+        while(i > -1 && array[i] > key) {
+
+            array[i + 1] = array[i];
+
+            i = i - 1;
+
+            array[i + 1] = key;
+        }
+    }
+    sortedArray = array;
+}
+
+void InsertionSort::printSortedArray() {
+    std::cout << "Unsorted Array: ";
+
+    for (int i : unsortedArray) {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
+    std::cout << "Sorted array: ";
+    for (int i : sortedArray) {
+        std::cout << i << " ";
+    }
+}
+
+std::vector<int> InsertionSort::getSortedArray() {
+    return sortedArray;
+}
+
 
--- cpp/src/sorting/InsertionSort.hpp
+++ cpp/src/sorting/InsertionSort.hpp
@@ -1,8 +1,20 @@
 #ifndef SORTINGALGORITHMS_INSERTIONSORT_HPP
 #define SORTINGALGORITHMS_INSERTIONSORT_HPP
+
+#include <vector>
+#include <iostream>
+#include "../helperClasses/Input.hpp"
+
 class InsertionSort {
+
 public:
+    InsertionSort();
+    void doInsertionSort(std::vector<int>);
+    void printSortedArray();
+    std::vector<int> getSortedArray();
 
 private:
+    std::vector<int> sortedArray;
+    std::vector<int> unsortedArray;
 };
 #endif //SORTINGALGORITHMS_INSERTIONSORT_HPP
